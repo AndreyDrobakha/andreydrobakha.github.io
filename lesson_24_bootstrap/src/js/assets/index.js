a9add9168767cb6d0d5790e47cf5f8cc2498783e
andrey_drobakha <gerrard881202@gmail.com>
2018-12-04 14:04:06 +0200

    homework_15_done

--- lesson_24_bootstrap/src/js/assets/index.js
+++
@@ -1,6 +0,0 @@
-import styles from '../../scss/style.scss';
-
-
-export {
-	styles
-}
---
+++ lesson_26_js_events/homework_15_events/src/js/script.js
@@ -0,0 +1,23 @@
+;(function(){
+  'use strict';
+
+  document.addEventListener("keydown", (event) => {
+    if (event.keyCode == 69) {
+      if (event.keyCode == 16)
+        console.log("Продолжаем!");
+    }
+  })
+
+ /* document.onkeydown = function(e) {
+      if ((e.ctrlKey && e.keyCode == 'M'.charCodeAt(0)) && !area.offsetHeight) {
+        console.log("Продолжаем!")
+        return false;
+      }
+    }*/
+
+    });
+
+
+
+
+
