e95feae65233a87955bb786fa1cc852aca7a22e1
andrey_drobakha <gerrard881202@gmail.com>
2019-01-19 10:06:56 +0200

    exam_3_added

---
+++ exam_2/app/js/script.js
@@ -0,0 +1,30 @@
+'use strict';
+/*const form = $('#contactForm');
+form.on('submit', (e) => {
+	e.preventDefault();
+});
+
+const servicesTexts = $('.grid-item__text');
+
+console.log(servicesTexts);
+
+
+
+servicesTexts.map = [].map;
+servicesTexts.forEach = [].forEach;
+
+const arr = servicesTexts.map(el => {
+	let a = el.innerText.split('');
+	if(a.length < 85) return el.innerText;
+	let newA = a.slice(0, 85);
+	let index = newA.lastIndexOf(' ');
+	return newA.slice(0, index).join('') + '...';
+	
+})
+
+
+
+servicesTexts.forEach((el, idx) => {
+	el.innerText = arr[idx];
+});*/
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbInNjcmlwdC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBIiwic291cmNlc0NvbnRlbnQiOlsiJ3VzZSBzdHJpY3QnO1xuLypjb25zdCBmb3JtID0gJCgnI2NvbnRhY3RGb3JtJyk7XG5mb3JtLm9uKCdzdWJtaXQnLCAoZSkgPT4ge1xuXHRlLnByZXZlbnREZWZhdWx0KCk7XG59KTtcblxuY29uc3Qgc2VydmljZXNUZXh0cyA9ICQoJy5ncmlkLWl0ZW1fX3RleHQnKTtcblxuY29uc29sZS5sb2coc2VydmljZXNUZXh0cyk7XG5cblxuXG5zZXJ2aWNlc1RleHRzLm1hcCA9IFtdLm1hcDtcbnNlcnZpY2VzVGV4dHMuZm9yRWFjaCA9IFtdLmZvckVhY2g7XG5cbmNvbnN0IGFyciA9IHNlcnZpY2VzVGV4dHMubWFwKGVsID0+IHtcblx0bGV0IGEgPSBlbC5pbm5lclRleHQuc3BsaXQoJycpO1xuXHRpZihhLmxlbmd0aCA8IDg1KSByZXR1cm4gZWwuaW5uZXJUZXh0O1xuXHRsZXQgbmV3QSA9IGEuc2xpY2UoMCwgODUpO1xuXHRsZXQgaW5kZXggPSBuZXdBLmxhc3RJbmRleE9mKCcgJyk7XG5cdHJldHVybiBuZXdBLnNsaWNlKDAsIGluZGV4KS5qb2luKCcnKSArICcuLi4nO1xuXHRcbn0pXG5cblxuXG5zZXJ2aWNlc1RleHRzLmZvckVhY2goKGVsLCBpZHgpID0+IHtcblx0ZWwuaW5uZXJUZXh0ID0gYXJyW2lkeF07XG59KTsqLyJdLCJmaWxlIjoic2NyaXB0LmpzIn0=
