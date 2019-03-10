57385b9ca514b7b1d43a914485f8658f0c3f9dbe
andrey_drobakha <gerrard881202@gmail.com>
2019-03-10 10:27:53 +0200

    exam_3_respons

--- exam_3/src/js/script.js
+++ exam_3/src/js/script.js
@@ -8,10 +8,10 @@
 		window.msRequestAnimationFrame;
 		window.requestAnimationFrame = requestAnimationFrame;
 
-		$('.carousel').carousel({
-			interval: 3000,
-
-		})
+		// $('.carousel').carousel({
+		// 	interval: 3000,
+		// 	autoplay: false,
+		// })
 		
 		$('.team__row').slick({
 			infinite: true,
@@ -56,47 +56,119 @@
 			}
 		};
 
-		const mobileMenuButton = $('.navbar-toggler');
-		const mobileMenu = $('#navbar');
+		// $(function () { 
+		// 	$('.navigation__link').each(function () {
+		// 		var location = window.location.href;
+		// 		var link = this.href; 
+		// 		if(location == link) {
+		// 			$(this).addClass('active');
+		// 		}
+		// 	});
+		// });
 
-		mobileMenuButton.on('click', function() {
+		// $(function () {
+		// 	var location = window.location.href;
+		// 	var cur_url = '/' + location.split('/').pop();
+
+		// 	$('.navigation__item').each(function () {
+		// 		var link = $(this).find('a').attr('href');
+
+		// 		if (cur_url == link)
+		// 		{
+		// 			$(this).addClass('active');
+		// 		}
+		// 	});
+		// });
+
+		// Cache selectors
+		var lastId,
+		topMenu = $("#menu"),
+		topMenuHeight = topMenu.outerHeight() + 15,
+  // All list items
+  menuItems = topMenu.find("a"),
+  // Anchors corresponding to menu items
+  scrollItems = menuItems.map(function() {
+  	var item = $($(this).attr("href"));
+  	if (item.length) {
+  		return item;
+  	}
+  });
+  
+// // Bind click handler to menu items
+// // so we can get a fancy scroll animation
+// menuItems.click(function(e) {
+//   var href = $(this).attr("href"),
+//     offsetTop = href === "#" ? 0 : $(href).offset().top - topMenuHeight + 1;
+//   $('html, body').stop().animate({
+//     scrollTop: offsetTop
+//   }, 300);
+//   e.preventDefault();
+// });
+
+// Bind to scroll
+$(window).scroll(function() {
+  // Get container scroll position
+  var fromTop = $(this).scrollTop() + topMenuHeight;
+  
+  // Get id of current scroll item
+  var cur = scrollItems.map(function() {
+  	if ($(this).offset().top < fromTop)
+  		return this;
+  });
+  // Get the id of the current element
+  cur = cur[cur.length - 1];
+  var id = cur && cur.length ? cur[0].id : "";
+  if (lastId !== id) {
+  	lastId = id;
+    // Set/remove active class
+    menuItems
+    .parent().removeClass("active")
+    .end().filter("[href='#" + id + "']").parent().addClass("active");
+}
+});
+
+
+const mobileMenuButton = $('.navbar-toggler');
+const mobileMenu = $('#navbar');
+
+mobileMenuButton.on('click', function() {
 			// this.style.display='none' 
 			mobileMenu.toggleClass('open');
 		});
 
-		$('.navigation__link').on('click', function(e){
-			e.preventDefault();
-			const top = $($(this).attr('href'))[0].offsetTop;
-			window.scrollTo({
-				top: top,
-				behavior: 'smooth'
-			});
-		});
+$('.navigation__link').on('click', function(e){
+	e.preventDefault();
+	const top = $($(this).attr('href'))[0].offsetTop;
+	window.scrollTo({
+		top: top,
+		behavior: 'smooth'
+	});
+});
 
-		$('.arrow-up').on('click', () => {
-			window.scrollTo({
-				top: 0,
-				behavior: 'smooth'
-			});
-		});
+$('.arrow-up').on('click', () => {
+	window.scrollTo({
+		top: 0,
+		behavior: 'smooth'
+	});
+});
 
-		const keys = {37: 1, 38: 1, 39: 1, 40: 1};
+const keys = {37: 1, 38: 1, 39: 1, 40: 1};
 
-		function preventDefault(e) {
-			e = e || window.event;
-			if (e.preventDefault)
-				e.preventDefault();
-			e.returnValue = false;
-		}
+function preventDefault(e) {
+	e = e || window.event;
+	if (e.preventDefault)
+		e.preventDefault();
+	e.returnValue = false;
+}
 
-		function preventDefaultForScrollKeys(e) {
-			if (keys[e.keyCode]) {
-				preventDefault(e);
-				return false;
-			}
-		}
+function preventDefaultForScrollKeys(e) {
+	if (keys[e.keyCode]) {
+		preventDefault(e);
+		return false;
+	}
+}
 
-		function disableScroll() {
+function disableScroll() {
   if (window.addEventListener) // older FF
   	window.addEventListener('DOMMouseScroll', preventDefault, false);
   window.onwheel = preventDefault; // modern standard
@@ -112,9 +184,9 @@ function enableScroll() {
 	window.onwheel = null;
 	window.ontouchmove = null;
 	document.onkeydown = null;
-}
+};
 
-;(function($) {
+(function($) {
 	'use strict';
 	$(window).on('load', function() {
 		var map;
